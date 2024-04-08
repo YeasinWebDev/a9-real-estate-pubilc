ba02592c4d3beec17de52f6d8f3a56f45984956e
YeasinWebDev <ar9485727@gmail.com>
2024-04-08 09:50:28 +0600

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { app } from './firebaseCofig'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-7xl'>hi</h1>
+    </div>
+  )
+}
+
+export default App
